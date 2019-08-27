d33594a0be9d8c44dd7d9f5b6e1e942b3d63c620
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2019-08-27 18:30:22 +0400

    lesson 3- Adding Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <navbar></navbar>
     </div>
     <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  components: {
+    navbar: Navbar
+  }
+};
+</script>
+import { Script } from "vm";
+
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
