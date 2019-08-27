05b3d3ca9857dec5cc051e691fe5b9644032dbc5
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2019-08-27 18:37:05 +0400

    lesson 3- Adding Navbar - fix spelling mistake in template word

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,31 @@
 <template>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Vue Laravel API</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
-      
-      <li class="nav-item">
-        <a  class="btn btn-primary my-1 btn-block" href="#">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="btn btn-success my-1 ml-1  btn-block" href="#" tabindex="-1" aria-disabled="true">Register</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-</templates>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Vue Laravel API</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="btn btn-primary my-1 btn-block" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="btn btn-success my-1 ml-1 btn-block"
+            href="#"
+            tabindex="-1"
+            aria-disabled="true"
+          >Register</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
